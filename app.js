d3fc91889679aa46e0cc46e0747ae3465cd7fd45
Daniel Leal <daniel.leal@my.scitexas.edu>
2017-09-07 08:19:39 +0000

    Created function to determine if the letters typed make up a word / Created function that increments the word count if letters typed make up a word

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-// [x] - Need to specify that we want the window page to load first before we run our JS code inside our function.
+// [x] - Add onload event to specify that we want the window page to load first before we run our JS code. 
 // [x] - Create a variable that calls an input element by id.
 // [x] - Create variables that will keep track of the data we are intersted in.
 // [x] - Add eventListener to the calculate button so when user clicks on the button it will run a function.
  window.onload = function (){
+    
+    
      var input = document.getElementById('input');
      var calculate = document.getElementById('calculate');
+     
      
      var totalWords = 0;
      var totalSentences = 0;
@@ -13,8 +16,29 @@
      
      calculate.addEventListener('click', function(){
       var textToAnalyze = input.value;
-      console.log(textToAnalyze);
+      for(var i = 0; i < textToAnalyze.length; i++){
+       if (isWord(textToAnalyze[i])){
+        totalWords++;
+        incrementWordCount(textToAnalyze[i]);
+       };
+      };
       
+      document.getElementById(totalWords).innerHTML = totalWords;
      });
+     
+     
+     function isWord(letter){
+      
+       return letter == 'a' || letter== 'b' || letter == 'c'|| letter == 'd'||  letter == 'e'|| letter == 'f' ||  letter == 'g'||  letter == 'h'||  letter == 'i'||  letter == 'j'||  letter == 'k'||  letter == 'l'||  letter == 'm'||  letter == 'n'||  letter == 'o'||  letter == 'p'||  letter == 'q'||  letter == 'r'||  letter == 's'||  letter == 't'||  letter == 'u'||  letter == 'v'||  letter == 'w'||  letter == 'x'||  letter == 'y'||  letter == 'z';
+      
+     };
+     
+     function incrementWordCount(letter){
+      if (letter == 'a' || letter == 'b' || letter == 'c' || letter == 'd' || letter == 'e' || letter == 'f' || letter == 'g' || letter == 'h' || letter == 'i' || letter == 'j' ||letter == 'k' || letter == 'l' || letter == 'm' || letter == 'n' || letter == 'o' || letter == 'p' || letter == 'q' || letter == 'r' || letter == 's' || letter == 't' || letter == 'u' || letter == 'v' || letter == 'w' || letter == 'x' || letter == 'y' || letter == 'z') {
+       totalWords++;
+      };
+      
+      
+     };
      
  };
